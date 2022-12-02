03ef3a5628e821c8e21d8acd15bc0da29d857809
Bernardo Alecrim <bernardo@alecrim.dev>
2022-12-02 13:27:58 +0000

    Initial Commit

---
+++ serene/sereneApp.swift
@@ -0,0 +1,17 @@
+//
+//  sereneApp.swift
+//  serene
+//
+//  Created by Bernardo Alecrim on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct sereneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
